a1c828ffaa583551c8e576c1ad01591aaa2793ce
tejkarandikar <tejmkarandikar@gmail.com>
2026-07-07 19:22:30 +0530

    Modify build.gradle.kts for dependency updates
    Updated dependencies and adjusted build features in the Gradle script.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,46 +1,86 @@
 plugins {
+
     id("com.android.application")
+
     id("org.jetbrains.kotlin.android")
+
 }
 
 android {
+
     namespace = "tejapps.bioanalytical"
 
     compileSdk = 35
 
     defaultConfig {
+
         applicationId = "tejapps.bioanalytical"
+
         minSdk = 24
+
         targetSdk = 35
+
         versionCode = 1
+
         versionName = "1.0"
+
+        testInstrumentationRunner =
+            "androidx.test.runner.AndroidJUnitRunner"
+
     }
 
     buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
 
-    buildFeatures {
-        viewBinding = true
+        release {
+
+            isMinifyEnabled = false
+
+            proguardFiles(
+
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
+
+                "proguard-rules.pro"
+
+            )
+
+        }
+
     }
 
     compileOptions {
+
         sourceCompatibility = JavaVersion.VERSION_17
+
         targetCompatibility = JavaVersion.VERSION_17
+
     }
 
     kotlinOptions {
+
         jvmTarget = "17"
+
     }
+
+    buildFeatures {
+
+        viewBinding = true
+
+    }
+
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.16.0")
+
+    implementation("androidx.appcompat:appcompat:1.7.1")
+
     implementation("com.google.android.material:material:1.12.0")
+
+    implementation("androidx.activity:activity-ktx:1.10.1")
+
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
 }
