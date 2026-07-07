e591a442dffec7450b82ec2a839ad9ba7daf720a
tejkarandikar <tejmkarandikar@gmail.com>
2026-07-07 18:19:45 +0530

    Implement BookmarkManager for managing bookmarks

---
+++ app/src/main/java/tejapps/bioanalytical/bookmarks/BookmarkManager.kt
@@ -0,0 +1,55 @@
+package tejapps.bioanalytical.bookmarks
+
+import android.content.Context
+import org.json.JSONArray
+
+class BookmarkManager(context: Context) {
+
+    private val preferences =
+        context.getSharedPreferences(
+            "bookmarks",
+            Context.MODE_PRIVATE
+        )
+
+    fun addBookmark(bookmark: Bookmark) {
+
+        val array = JSONArray(
+            preferences.getString(
+                "items",
+                "[]"
+            )
+        )
+
+        array.put(bookmark.assetFile)
+
+        preferences.edit()
+            .putString(
+                "items",
+                array.toString()
+            )
+            .apply()
+
+    }
+
+    fun getBookmarks(): List<String> {
+
+        val array = JSONArray(
+            preferences.getString(
+                "items",
+                "[]"
+            )
+        )
+
+        val list = mutableListOf<String>()
+
+        for(i in 0 until array.length()){
+
+            list.add(array.getString(i))
+
+        }
+
+        return list
+
+    }
+
+}
