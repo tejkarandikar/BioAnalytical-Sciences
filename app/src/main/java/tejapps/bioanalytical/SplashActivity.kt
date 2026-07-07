54fac467036ed29fb3251c16c8b557234a68d5b1
tejkarandikar <tejmkarandikar@gmail.com>
2026-07-07 16:15:49 +0530

    Add SplashActivity for app startup screen

---
+++ app/src/main/java/tejapps/bioanalytical/SplashActivity.kt
@@ -0,0 +1,26 @@
+package tejapps.bioanalytical
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash)
+
+        window.decorView.postDelayed({
+
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
+
+            finish()
+
+        },1500)
+
+    }
+
+}
