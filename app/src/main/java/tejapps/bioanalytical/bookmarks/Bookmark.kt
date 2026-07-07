b38cf4ed2a94c00449d6f77b58e873d0407abf82
tejkarandikar <tejmkarandikar@gmail.com>
2026-07-07 18:32:04 +0530

    Add timestamp property to Bookmark data class

--- app/src/main/java/tejapps/bioanalytical/bookmarks/Bookmark.kt
+++ app/src/main/java/tejapps/bioanalytical/bookmarks/Bookmark.kt
@@ -1,9 +1,13 @@
 package tejapps.bioanalytical.bookmarks
 
+package tejapps.bioanalytical.bookmarks
+
 data class Bookmark(
 
     val title: String,
 
-    val assetFile: String
+    val assetFile: String,
+
+    val timestamp: Long
 
 )
