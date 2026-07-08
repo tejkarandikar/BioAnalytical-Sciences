2c955afc21e0c4080118cb753272b536b35e9e17
tejkarandikar <tejmkarandikar@gmail.com>
2026-07-08 16:00:36 +0530

    Implement swipe-to-remove for bookmarks in RecyclerView
    Added swipe-to-remove functionality for bookmarks.

--- app/src/main/java/tejapps/bioanalytical/bottomsheet/BookmarksBottomSheet.kt
+++ app/src/main/java/tejapps/bioanalytical/bottomsheet/BookmarksBottomSheet.kt
@@ -37,6 +37,7 @@ class BookmarksBottomSheet(
     onBookmarkSelected(bookmark)
 
     dismiss()
+    
 
 }
 
@@ -49,7 +50,57 @@ class BookmarksBottomSheet(
             bookmarkManager.clearBookmarks()
 
             adapter.submitList(emptyList())
+val swipeHandler =
 
+    object : ItemTouchHelper.SimpleCallback(
+
+        0,
+
+        ItemTouchHelper.LEFT
+
+    ) {
+
+        override fun onMove(
+
+            recyclerView: RecyclerView,
+
+            viewHolder: RecyclerView.ViewHolder,
+
+            target: RecyclerView.ViewHolder
+
+        ): Boolean {
+
+            return false
+
+        }
+
+        override fun onSwiped(
+
+            viewHolder: RecyclerView.ViewHolder,
+
+            direction: Int
+
+        ) {
+
+            val position = viewHolder.adapterPosition
+
+            val bookmark =
+
+                bookmarkManager
+
+                    .getBookmarks()[position]
+
+            bookmarkManager.removeBookmark(bookmark)
+
+            adapter.submitList(
+
+                bookmarkManager.getBookmarks()
+
+            )
+
+        }
+
+    }
         }
 
     }
