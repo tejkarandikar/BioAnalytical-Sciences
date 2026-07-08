95a2e90078b9a9b5a1a658eed57e02009b966f85
tejkarandikar <tejmkarandikar@gmail.com>
2026-07-08 14:41:43 +0530

    Add layoutId parameter to BaseBottomSheet class

--- app/src/main/java/tejapps/bioanalytical/bottomsheet/BaseBottomSheet.kt
+++ app/src/main/java/tejapps/bioanalytical/bottomsheet/BaseBottomSheet.kt
@@ -1,5 +1,29 @@
 package tejapps.bioanalytical.bottomsheet
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-abstract class BaseBottomSheet : BottomSheetDialogFragment()
+abstract class BaseBottomSheet(
+    @LayoutRes
+    private val layoutId: Int
+) : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        return inflater.inflate(
+            layoutId,
+            container,
+            false
+        )
+
+    }
+
+}
