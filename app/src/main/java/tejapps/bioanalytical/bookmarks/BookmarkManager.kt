c003c8b585a515b41d06995f448d42239aac5e5d
tejkarandikar <tejmkarandikar@gmail.com>
2026-07-08 12:51:28 +0530

    Refactor addBookmark and getBookmarks methods

--- app/src/main/java/tejapps/bioanalytical/bookmarks/BookmarkManager.kt
+++ app/src/main/java/tejapps/bioanalytical/bookmarks/BookmarkManager.kt
@@ -21,75 +21,57 @@ class BookmarkManager(
         )
 
 }
-    fun addBookmark(
+   fun addBookmark(bookmark: Bookmark) {
 
-    bookmark: Bookmark
+    val bookmarks = getBookmarks().toMutableList()
 
-) {
+    // Remove existing bookmark for the same chapter
+    bookmarks.removeAll {
 
-    val array = JSONArray(
+        it.chapter.menuId == bookmark.chapter.menuId
 
-        preferences.getString(
+    }
 
-            "items",
+    // Add the newest bookmark at the top
+    bookmarks.add(0, bookmark)
 
-            "[]"
+    // Limit to the latest 100 bookmarks
+    if (bookmarks.size > 100) {
 
+        bookmarks.subList(100, bookmarks.size).clear()
+
+    }
+
+    val array = JSONArray()
+
+    bookmarks.forEach { item ->
+
+        val obj = JSONObject()
+
+        obj.put(
+            "menuId",
+            item.chapter.menuId
         )
 
-    )
+        obj.put(
+            "timestamp",
+            item.timestamp
+        )
 
-    val obj = JSONObject()
+        array.put(obj)
 
-    obj.put(
-
-        "title",
-
-        bookmark.chapter.title
-
-    )
-
-    obj.put(
-
-        "assetFile",
-
-        bookmark.chapter.assetFile
-
-    )
-
-    obj.put(
-
-        "menuId",
-
-        bookmark.chapter.menuId
-
-    )
-
-    obj.put(
-
-        "timestamp",
-
-        bookmark.timestamp
-
-    )
-
-    array.put(obj)
+    }
 
     preferences.edit()
-
         .putString(
-
             "items",
-
             array.toString()
-
         )
-
         .apply()
 
 }
 
-   fun getBookmarks(): List<Bookmark> {
+  fun getBookmarks(): List<Bookmark> {
 
     val bookmarks = mutableListOf<Bookmark>()
 
@@ -101,27 +83,25 @@ class BookmarkManager(
 
         val obj = array.getJSONObject(i)
 
-        val chapter = Chapter(
+        val menuId = obj.getInt("menuId")
 
-            menuId = obj.getInt("menuId"),
+        val chapter = ChapterRepository.getChapterByMenuId(menuId)
 
-            title = obj.getString("title"),
+        if (chapter != null) {
 
-            assetFile = obj.getString("assetFile")
+            bookmarks.add(
 
-        )
+                Bookmark(
 
-        bookmarks.add(
+                    chapter = chapter,
 
-            Bookmark(
+                    timestamp = obj.getLong("timestamp")
 
-                chapter = chapter,
-
-                timestamp = obj.getLong("timestamp")
+                )
 
             )
 
-        )
+        }
 
     }
 
